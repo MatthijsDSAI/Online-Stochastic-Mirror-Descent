ad98c25bb0b4d8e85cb14360a5823ad7fc2faf5b
MatthijsDSAI <72268494+MatthijsDSAI@users.noreply.github.com>
2023-03-03 13:13:18 +0100

    Changed distribution

--- Run.py
+++ Run.py
@@ -2,7 +2,7 @@ from Graphs import *
 from Algorithms import *
 
 # generate a gridgraph where the weights for edges are pulled from the below defined distribution
-distribution = lambda: round(np.random.normal(2, 1), 2)     # select the distribution to be used
+distribution = lambda: round(np.random.normal(20, 5), 2)     # select the distribution to be used
 gg = GridGraph(3, 3, distribution)
 
 algs = Algorithms(gg) # initialize algorithms class for the generated graph
@@ -20,7 +20,7 @@ encoded_paths = algs.encode(paths)
 print("\nEncoded paths:\n" + str(encoded_paths))
 
 # run the EXP(2) algorithm
-algs.exp2(0.1, paths=encoded_paths, rounds=15)
+algs.exp2(0.1, paths=encoded_paths, rounds=200)
 
 # visualize the graph
 gv = GridGraphVisualizer(gg) # for the visualization, each edge has a tuple for the label with the following structure: (id, weight)
